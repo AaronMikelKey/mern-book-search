a29bd784eda3986f71bf306cd6722f5b31a0f0f9
Aaron Key <AaronMikelKey@gmail.com>
2022-10-24 13:48:15 -0400

    Add Apollo schema, update some packages

--- server/server.js
+++ server/server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express')
 
+const { typeDefs, resolvers } = require('./schemas')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({app})
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -17,5 +29,12 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
+}
+
+
+startApolloServer(typeDefs, resolvers)
